9a8c31d838de4ebce5bf1abd75990b9542beedaf
tosu <timo42@proton.me>
2023-08-27 02:47:42 +0200

    Add rank_mirrors.sh script

---
+++ .local/bin/rank_mirrors.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -s "https://archlinux.org/mirrorlist/?country=DE&protocol=https&use_mirror_status=on" \
+  | sed -e 's/^#Server/Server/' -e '/^#/d' \
+  | rankmirrors -n 10 -
+
+# output to /etc/pacman.d/mirrorlist
