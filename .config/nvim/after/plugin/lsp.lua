76432d511b2f5c5602877ec7cfaf42ca2581210d
tosu <timo42@proton.me>
2024-10-04 15:11:08 +0200

    Save

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -38,8 +38,8 @@ local cmp_action = require('lsp-zero').cmp_action()
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-t>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	-- ['<C-Space>'] = cmp.mapping.complete(),
 })
 --
 lsp.set_preferences({
